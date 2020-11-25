0ba93a0f1b23b5d6bd1119ce20d8d1401e2ffb2e
digitbase <digitbase@gmail.com>
2020-11-25 17:14:52 +0800

    redux

--- src/TodoList.js
+++ src/TodoList.js
@@ -1,28 +1,65 @@
-import React, { Component } from "react";
+import React from "react";
 import { connect } from "react-redux";
 
-class TodoList extends Component {
-    constructor(props) {
-        super(props);
-    }
-    render() {
-        return (
+const TodoList = (props) => {
+    let { inputValue, inputChange, clickBtn, delItem, list } = props;
+    return (
+        <div>
             <div>
-                <div>
-                    <input value={this.props.inputValue} />
-                </div>
-                <ul>
-                    <li>list</li>
-                </ul>
+                <input value={inputValue} onChange={inputChange} />
+                <button onClick={clickBtn}>提交</button>
             </div>
-        );
-    }
-}
+            <ul>
+                {list.map((item, index) => {
+                    return (
+                        <li
+                            onClick={() => {
+                                delItem(index);
+                            }}
+                            key={index}
+                        >
+                            {item}
+                        </li>
+                    );
+                })}
+            </ul>
+        </div>
+    );
+};
 
 const stateToProps = (state) => {
     return {
         inputValue: state.inputValue,
+        list: state.list,
     };
 };
 
-export default connect(stateToProps, null)(TodoList);
+const dispatchToProps = (dispatch) => {
+    return {
+        inputChange(e) {
+            console.log(e.target.value);
+            //this.props.inputValue = "";
+            let action = {
+                type: "change_input",
+                value: e.target.value,
+            };
+            dispatch(action);
+        },
+        clickBtn() {
+            let action = {
+                type: "btn_click",
+            };
+            dispatch(action);
+        },
+        delItem(index) {
+            // console.log(index);
+            let action = {
+                type: "del_item",
+                index: index,
+            };
+            dispatch(action);
+        },
+    };
+};
+
+export default connect(stateToProps, dispatchToProps)(TodoList);
--- src/store/reducer.js
+++ src/store/reducer.js
@@ -6,5 +6,25 @@ const defaultState = {
 };
 
 export default (state = defaultState, action) => {
+    if (action.type === "change_input") {
+        let newState = JSON.parse(JSON.stringify(state));
+        newState.inputValue = action.value;
+        return newState;
+    }
+
+    if (action.type === "btn_click") {
+        let newState = JSON.parse(JSON.stringify(state));
+        newState.list.push(newState.inputValue);
+        newState.inputValue = "";
+        console.log(action.value);
+        return newState;
+    }
+
+    if (action.type === "del_item") {
+        let newState = JSON.parse(JSON.stringify(state));
+        newState.list.splice(action.index, 1);
+        console.log(action.index);
+        return newState;
+    }
     return state;
 };
